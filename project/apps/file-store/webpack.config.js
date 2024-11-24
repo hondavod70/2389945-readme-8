7ed77f9e1afbe1573031bf1aa9c28951b2bfb8d1
hondavod70 <hondavod70@gmail.com>
2024-11-24 18:49:37 +0700

    Изменил архитектуру проекта

--- project/apps/file-store/webpack.config.js
+++ project/apps/file-store/webpack.config.js
@@ -3,7 +3,7 @@ const { join } = require('path');
 
 module.exports = {
   output: {
-    path: join(__dirname, '../../dist/apps/comment'),
+    path: join(__dirname, '../../dist/apps/file-store'),
   },
   plugins: [
     new NxAppWebpackPlugin({
