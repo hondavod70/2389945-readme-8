8184938287c0090abc041eb2011285c4ee81f67d
hondavod70 <hondavod70@gmail.com>
2024-12-08 21:56:01 +0700

    ++StorableEntity

--- project/libs/shared/core/src/index.ts
+++ project/libs/shared/core/src/index.ts
@@ -2,3 +2,5 @@ export { Entity } from './lib/base/entity';
 export * from './lib/types/auth-user.interface';
 export * from './lib/types/post.interface';
 export * from './lib/types/user.interface';
+export { StorableEntity } from './lib/interfaces/storable-entity.interface';
+export { EntityFactory } from './lib/interfaces/entity-factory.interface';
